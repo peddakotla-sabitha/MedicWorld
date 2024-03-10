b02f6695fa96c8a85f6a3d41adb50f839e17d1df
peddakotla-sabitha <162852027+peddakotla-sabitha@users.noreply.github.com>
2024-03-10 09:25:42 +0530

    Add files via upload

---
+++ z_droperidol.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+<title>droperidol-Medic World</title>
+<script src="https://kit.fontawesome.com/c4254e24a8.js" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"                                   integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-                          geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</head>
+<body style="background-color: black; color: white;">
+		<div class="container mt-2">
+			
+			<h2>What is droperidol?</h2><br><br>
+				Droperidol is a sedative, tranquilizer, and anti-nausea medicine<br><br>.
+
+				Droperidol is used to reduce nausea and vomiting caused by surgery or other medical procedures.<br><br>
+
+				Droperidol may also be used for purposes not listed in this medication guide.<br><br>
+
+				 <h3>Warnings</h3><br><br>
+				Tell your caregivers at once if you have sudden dizziness with fast or pounding heartbeats, fluttering in your chest, 					or trouble breathing.<br><br>
+
+				<h3>Before taking this medicine</h3><br><br>
+				You should not be treated with droperidol if you are allergic to it, or if you have a personal or family history of 					long QT syndrome.<br><br>
+
+				Tell your doctor if you have ever had:<br><br>
+
+				congestive heart failure;<br><br>
+
+				very slow heartbeats;<br><br>
+
+				high blood pressure;<br><br>
+
+				an electrolyte imbalance (such as low levels of potassium or magnesium in your blood);<br><br>
+
+				liver or kidney disease;<br><br>
+
+				pheochromocytoma (tumor of the adrenal gland); or<br><br>
+
+				alcoholism.<br><br>
+
+				<h3>How is droperidol given?</h3><br><br>
+				Before you receive droperidol, your heart function may need to be checked using an electrocardiograph or ECG 					(sometimes called an EKG).<br><br>
+
+				Droperidol is injected into a muscle, under the skin, or into a vein. You will receive this injection in a medical 					setting prior to and/or during your surgery or medical procedure.<br><br>
+
+				Your breathing, blood pressure, oxygen levels, kidney function, and other vital signs will be watched closely.<br><br>
+
+
+	
+	                <a href="medicinepage.php" style="text-decoration: none;"><button class="btn btn-outline-light text-light bg-dark mt-2 						mb-3 ">Go to home page</button></a>
+		</div>		
+
+
+</body>
+</html>
