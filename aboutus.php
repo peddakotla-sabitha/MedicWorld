d6490ae0a09eb24d18639a494f98a935bae8e1b6
PEDDAKOTLA SABITHA <sabithareddy0000@gmail.com>
2024-03-10 09:16:22 +0530

    contact us

---
+++ aboutus.php
@@ -0,0 +1,34 @@
+<html>
+	<body style="background-color: black; color: white;">
+		<div style="text-align: center;">
+			<h2>About Us</h2>
+				<h3>Mission Statement</h3><br>
+				"To empower patients with the knowledge to better manage their own healthcare and to improve consumer safety by 				assisting in the reduction of medication errors."<br><br>
+
+				Medic World is the largest, most widely visited, independent medicine information website available on the Internet. 					Our aim is to be the Internet’s most trusted resource for drug and related health information. We will achieve this 					aim by presenting independent, objective, comprehensive and up-to-date information in a clear and concise format for 					both consumers and healthcare professionals.<br><br>
+
+				The Medic World Database
+				The Medic World Drug Information Database is powered by several independent leading medical-information suppliers, 					including; American Society of Health-System Pharmacists, Cerner Multum and IBM Watson Micromedex. Individual drug (or 					drug-class) content compiled by these sources is peer reviewed and delivered by Medic World<br><br>
+
+				Medic World also publishes health content from other sources such as;<br><br>
+
+				Harvard Health Publications<br><br>
+				Articles include Harvard Health Topics A to Z and Harvard Health Decision Guides (used in our Symptom Checker). 				Harvard Health Publications is the publishing division of the Harvard Medical School of Harvard University and draws 					on the expertise of the 9,000 faculty physicians to provide authoritative and trustworthy consumer health 					information.<br><br>
+
+				Mayo Clinic<br><br>
+				Mayo Clinic articles provide detailed information about medical conditions and diseases. More than 3,300 physicians, 					scientists and researchers from Mayo Clinic share their expertise to empower you. Content includes definitions, 				symptoms, causes, risk factors, complications, tests and diagnosis, treatments and drugs, prevention and support 					options.<br><br>
+
+				DVMetrics - Animalytics<br><br>
+				The Medic World Veterinary Product Database is an online version of the Compendium of Veterinary Products published by 					DVMetrics. The Compendium of Veterinary Products is the animal health industry's largest and most comprehensive 				database of more than 6,000 pharmaceutical, biological, parasiticide and supplement product monographs. DVMetrics is 					the home for some of the most trusted resources used by veterinary professionals in North America including the North 					American Compendium of Veterinary Products and the Safety Data Sheet Library which has been available for over 30 and 					25 years, respectively.<br><br>
+
+				News<br><br>
+				The "Latest News" and "FDA Drug Alerts" sections are sourced from standard industry press releases, newspapers, online 					news sites, medical journals, and the FDA.<br><br>
+
+				Give Us Your Feedback<br><br>
+				The success of Medic World depends on the satisfaction of our visitors. We welcome your questions and suggestions as 					to how we can improve our services. You can contact us here.<br><br>
+				For answers to frequently asked questions about Drugs.com click here.<br><br>
+
+				Medic World is operated by the Drugsite Trust, a dedicated provider of Internet-based medical information.<br><br>
+		</div>
+	</body>
+</html>
